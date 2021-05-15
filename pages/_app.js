44df77e72dd0685f65fbaab8cb104ac94c72d488
dahiya07 <53432604+dahiya07@users.noreply.github.com>
2021-05-15 18:19:17 +0530

    Delete pages directory

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "../styles/globals.css";
-// import "../utils/socketFunctionality";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
